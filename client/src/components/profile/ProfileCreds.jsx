3dbca2bde5c5f7a2ed9dddc84cdd3a6c8f74a2a3
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-04-03 19:10:54 +0200

    Spacing on header

--- client/src/components/profile/ProfileCreds.jsx
+++ client/src/components/profile/ProfileCreds.jsx
@@ -5,7 +5,7 @@ const ProfileCreds = ({ experience, education }) => {
     <li key={exp._id} className="list-group-item">
       <h4>{exp.company}</h4>
       <p>
-        <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
+        <Moment format="YYYY/MM/DD">{exp.from}</Moment> -{" "}
         {exp.to === null ? (
           " Current"
         ) : (
@@ -39,7 +39,7 @@ const ProfileCreds = ({ experience, education }) => {
     <li key={edu._id} className="list-group-item">
       <h4>{edu.school}</h4>
       <p>
-        <Moment format="YYYY/MM/DD">{edu.from}</Moment> -
+        <Moment format="YYYY/MM/DD">{edu.from}</Moment> -{" "}
         {edu.to === null ? (
           " Current"
         ) : (
