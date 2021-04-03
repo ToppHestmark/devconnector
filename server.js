99c5d109cac3061167cab89e808f2f2cb2aeae8e
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-04-04 00:37:27 +0200

    Replaced the deployment method

--- server.js
+++ server.js
@@ -10,14 +10,11 @@ require("./startup/config")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
 
-// Set server static asset if in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build"));
+});
 
 const port = process.env.PORT || 5500;
 app.listen(port, () => console.log(`Listening on port: ${port}`));
