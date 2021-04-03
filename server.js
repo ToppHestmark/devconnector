fb5077a78db98d24a1678e0a911c45d4cfaad543
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-04-03 19:41:29 +0200

    Preparing for deploy

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const app = require("express")();
-
+const express = require("express");
 require("dotenv").config();
+const path = require("path");
+
+// Set server static asset if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 require("./startup/bodyParser")(app);
 require("./startup/config")(app);
 require("./startup/routes")(app);
