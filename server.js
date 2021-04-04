afb80720aa8dc25e98e2656b9a13709a79a2ea06
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-04-04 10:00:22 +0200

    Deploy day 2

--- server.js
+++ server.js
@@ -10,11 +10,14 @@ require("./startup/config")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+// Set server static asset if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5500;
 app.listen(port, () => console.log(`Listening on port: ${port}`));
